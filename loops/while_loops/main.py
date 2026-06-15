974dd763ba4d35a95d6be73134871ef92f7b9f64
diyaajain <>
2026-06-15 07:44:33 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,11 @@
+start_number = 5
+countdown_values = []
+
+print("Countdown complete!")
+
+while start_number >= 1:
+    countdown_values.append(start_number)
+    start_number = start_number - 1
+    print(countdown_values)
+
+print("Discount countdown complete")
