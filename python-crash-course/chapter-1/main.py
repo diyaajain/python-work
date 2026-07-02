c2625fd23fd8c77ff052bb41e3621ed639d138a6
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-02 12:06:23 +0530

    segregated into chapters

---
+++ python-crash-course/chapter-1/main.py
@@ -0,0 +1,5 @@
+message = "hello world"
+print(message)
+
+message1 = "hello python crash course"
+print(message1)
--- python-crash-course/main.py
+++
@@ -1,4 +0,0 @@
-message = "hello world"
-print(message)
-
-message1 = "hello python crash course"
