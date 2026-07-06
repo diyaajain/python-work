2c12d14386a04807eff1ed6fa07286f9e2cc7e0f
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-06 13:21:59 +0530

    Create _cases.py

---
+++ python-crash-course/chapter-2/_cases.py
@@ -0,0 +1,15 @@
+name = 'Eric'
+print("Hello " + name + ", would you like to learn some Python today?")
+print(f"Hello, {name.title()}!")
+print(f"Hello, {name.upper()}!")
+print(f"Hello, {name.lower()}!")
+famous_person = "Ranchoddas Shamaldas Chanchad"
+print(f"{famous_person} once said, \"All is WelL!\"")
+
+person = 'David Dobrik  '
+person = person.rstrip()
+print(person)
+
+nostarch_url = 'https://nostarch.com'
+nostarch_url = nostarch_url.removeprefix('https://')
+print(nostarch_url)
