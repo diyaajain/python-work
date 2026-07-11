98a16935dc4acb12cda28c5d8b8af6f44e32afbe
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-11 14:08:44 +0530

    Update motorcycles.py

--- python-crash-course/chapter-3/motorcycles.py
+++ python-crash-course/chapter-3/motorcycles.py
@@ -16,3 +16,11 @@ print(motorcycles)
 popped_motorcycle = motorcycles.pop()
 print(motorcycles)
 print(popped_motorcycle)
+
+last_owned = motorcycles.pop()
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
