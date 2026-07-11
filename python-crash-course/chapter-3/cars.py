ecd1a7659ec5f9bfe03f1c0f471090dc69d38b1b
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-11 14:19:54 +0530

    Update cars.py

--- python-crash-course/chapter-3/cars.py
+++ python-crash-course/chapter-3/cars.py
@@ -1,5 +1,15 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
+
 cars.sort()
 print(f'The sorted list is: {cars}')
+
 cars.sort(reverse=True)
 print(f'The reverse sorted list is: {cars}')
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.reverse()
+print(f'The reversed list is: {cars}')
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+length = len(cars)
+print(f'The length of the list is: {length}')
