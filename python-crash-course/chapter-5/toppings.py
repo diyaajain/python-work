04d2e5ed50e814cd5fe2cf330de4319c3f12bc53
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-18 14:38:18 +0530

    Update toppings.py

--- python-crash-course/chapter-5/toppings.py
+++ python-crash-course/chapter-5/toppings.py
@@ -26,3 +26,21 @@ if age >= 18:
 else:
     print("Sorry, you are too young to vote.")
     print("Please register to vote as soon as you turn 18!")
+
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5.")
+else:
+    print("Your admission cost is $10.")
+
+age = 34
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+
+print(f"Your admission cost is ${price}.")
