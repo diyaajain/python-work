1c1543988f6318ed3f2ef397e40207a9ecdf52b8
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-20 11:57:04 +0530

    Update toppings.py

--- python-crash-course/chapter-5/toppings.py
+++ python-crash-course/chapter-5/toppings.py
@@ -54,3 +54,12 @@ if 'extra cheese' in requested_toppings:
     print("Adding extra cheese.")
 
 print("\nFinished making your pizza!")
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
