7cc7009e1c0d2476ff690a9c3726e0750b820c15
Diya Jain <82181956+diyaajain@users.noreply.github.com>
2026-07-22 15:40:21 +0530

    Update alien.py

--- python-crash-course/chapter-6/alien.py
+++ python-crash-course/chapter-6/alien.py
@@ -10,3 +10,8 @@ print(alien_0)
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
+
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}.")
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
